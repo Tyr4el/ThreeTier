5cacb6f37fcae07a9a7b714493b535f1150e1567
Nick Conklin <nconkl1@outlook.com>
2018-03-08 16:00:24 -0500

    Modified the println statements

--- WEB-INF/classes/Main.java
+++ WEB-INF/classes/Main.java
@@ -1,7 +1,28 @@
 import javax.servlet.Servlet;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-public class Main extends HttpServlet{
+public class Main extends HttpServlet {
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+            IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Output</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<p>Hello, this is a test</p>");
+        out.println("</body>");
+        out.println("</html>");
+
+    }
 
     public static void main(String[] args) {
 
