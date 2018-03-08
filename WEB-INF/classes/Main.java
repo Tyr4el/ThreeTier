2fd6982e1b96c3784950791e50bf9fc8820bc0b1
Nick Conklin <nconkl1@outlook.com>
2018-03-08 17:04:37 -0500

    Modified doPost in Main and modified form method and name in index

--- WEB-INF/classes/Main.java
+++ WEB-INF/classes/Main.java
@@ -1,26 +1,67 @@
 import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.UnavailableException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main extends HttpServlet {
 
+    private Connection connection;
+    private Statement statement;
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException,
             IOException {
+
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
 
-        out.println("<html>");
-        out.println("<head>");
-        out.println("<title>Output</title>");
-        out.println("</head>");
-        out.println("<body>");
-        out.println("<p>Hello, this is a test</p>");
-        out.println("</body>");
-        out.println("</html>");
+        String query = request.getParameter("sqlStatement").toLowerCase();
+        String[] querySplit = query.split(" ", 2);
+        String firstWord = querySplit[0];
+
+        init(); // Create the connection to the database
+
+        if (firstWord.equals("select")) {
+            try {
+                statement.executeQuery(query);
+            } catch (SQLException sql) {
+                sql.printStackTrace();
+            }
+        } else {
+            try {
+                statement.executeUpdate(query);
+            } catch (SQLException sql) {
+                sql.printStackTrace();
+            }
+        }
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,
+            IOException {
+
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+
+    }
+
+    public void init() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3310/project4", "root", "root");
+            statement = connection.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+            e.getMessage();
+        }
 
     }
 
